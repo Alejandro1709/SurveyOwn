7da1f40bc378bd5f3e99ed95c8d6d698137f69cf
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-05-03 14:34:07 -0500

    finish mockup header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import DashboardPage from './pages/DashboardPage'
-import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="container">
+      <Navbar />
       <DashboardPage />
     </div>
   )
