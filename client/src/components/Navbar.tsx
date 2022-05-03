1577f4f30ac1265c34b05a79587fe79f3319e582
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-05-03 14:43:49 -0500

    add navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,7 +3,12 @@ import './Navbar.css'
 
 function Navbar() {
   return (
-    <div className="Navbar">Navbar</div>
+    <div className="Navbar">
+      <nav role='navigation'>
+        <div className="Navbar-logo">ALForm</div>
+        <a href='#!'>Account</a>
+      </nav>
+    </div>
   )
 }
 
